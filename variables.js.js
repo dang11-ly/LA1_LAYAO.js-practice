3672686647d27c74df9c566a1fb8c35fbe71f5d0
dang11-ly <layaoamandayvelle@gmail.com>
2024-09-23 17:45:30 +0800

    Add files via upload

---
+++ variables.js.js
@@ -0,0 +1,11 @@
+// Variables
+var name = "Amanda Yvelle Bosque Layao";
+var whatDoYouWannaBecomeInYourLife = "Freelancer Video Editor";
+var gender = "Female";
+var twitterHandle = "Pszy.sza";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+
+console.log(gender);
+console.log(twitterHandle);
